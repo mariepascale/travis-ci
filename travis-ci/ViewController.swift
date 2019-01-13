df4d90bee5d72836d3a61aef732334bc7ca610cf
mariepascale <marie.pascale.chauvel@gmail.com>
2019-01-13 14:54:13 -0500

    Starter app for travis ci build

---
+++ travis-ci/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  travis-ci
+//
+//  Created by Marie-Pascale Chauvel on 1/11/19.
+//  Copyright © 2019 Marie-Pascale Chauvel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
